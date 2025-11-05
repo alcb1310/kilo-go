fcd6b31fb6ea6440c3236c4dce72d7887cb27537
Andres Court <andres@andrescourt.com>
2025-11-05 16:41:08 -0500

    Disable raw mode at exit

--- linux/raw.go
+++ linux/raw.go
@@ -2,21 +2,29 @@ package linux
 
 import (
 	"fmt"
+	"os"
 
 	"golang.org/x/sys/unix"
 )
 
-func EnableRawMode() error {
+func EnableRawMode() (func(), error) {
 	termios, err := unix.IoctlGetTermios(unix.Stdin, unix.TCGETS)
 	if err != nil {
-		return fmt.Errorf("EnableRawMode: error getting terminal flags: %w", err)
+		return nil, fmt.Errorf("EnableRawMode: error getting terminal flags: %w", err)
 	}
+
+	original := *termios
 
 	termios.Lflag &^= unix.ECHO
 
 	if err = unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, termios); err != nil {
-		return fmt.Errorf("EnableRawMode: error setting terminal flags: %w", err)
+		return nil, fmt.Errorf("EnableRawMode: error setting terminal flags: %w", err)
 	}
 
-	return nil
+	return func() {
+		if err = unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, &original); err != nil {
+			fmt.Fprintf(os.Stderr, "EnableRawMode: error restoring terminal flags: %s\n", err)
+			os.Exit(1)
+		}
+	}, nil
 }

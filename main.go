0e2dd21497ce3ab9e0ba6b0c12ce323246eefee5
Andres Court <andres@andrescourt.com>
2025-11-05 20:52:45 -0500

    Turn off all output processing

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	restoreFunc, err := linux.EnableRawMode()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Error: %s\r\n", err)
 		os.Exit(1)
 	}
 	defer restoreFunc()
@@ -24,14 +24,14 @@ func main() {
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: reading key from Stdin: %s\n", err)
+			fmt.Fprintf(os.Stderr, "Error: reading key from Stdin: %s\r\n", err)
 			os.Exit(1)
 		}
 
 		if b <= 0x1f || b == 0x7f { // This will make sure we've passed a control-key combo
-			fmt.Fprintf(os.Stdout, "%d\n", b)
+			fmt.Fprintf(os.Stdout, "%d\r\n", b)
 		} else {
-			fmt.Fprintf(os.Stdout, "%d (%c)\n", b, b)
+			fmt.Fprintf(os.Stdout, "%d (%c)\r\n", b, b)
 		}
 
 		if b == 'q' {

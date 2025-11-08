61696c7c7a37e829fd1bbb1e6ffdab2fdc9375fe
Andres Court <andres@andrescourt.com>
2025-11-08 08:24:19 -0500

    Tildes

--- editor/output.go
+++ editor/output.go
@@ -10,4 +10,14 @@ import (
 func (e *EditorConfig) editorRefreshScreen() {
 	fmt.Fprintf(os.Stdout, "%c[2J", utils.ESC)
 	fmt.Fprintf(os.Stdout, "%c[H", utils.ESC)
+
+	e.editorDrawRows()
+
+	fmt.Fprintf(os.Stdout, "%c[H", utils.ESC)
+}
+
+func (e *EditorConfig) editorDrawRows() {
+	for range 24 {
+		fmt.Fprintf(os.Stdout, "~\r\n")
+	}
 }

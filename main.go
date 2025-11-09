30ca847b6f43b36266ddf87ce78d6fa1f42574da
Andres Court <andres@andrescourt.com>
2025-11-08 19:14:22 -0500

    Enable logging

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/alcb1310/kilo-go/editor"
 	"github.com/alcb1310/kilo-go/linux"
+	"github.com/alcb1310/kilo-go/utils"
 )
 
 var restoreFunc func()
 
 func init() {
+	var f *os.File
 	var err error
+	userTempDir, _ := os.UserConfigDir()
+	if f, err = utils.CreateLoggerFile(userTempDir); err != nil {
+		utils.SafeExit(nil, err)
+	}
+
+	handlerOptions := &slog.HandlerOptions{}
+	handlerOptions.Level = slog.LevelDebug
+
+	loggerHandler := slog.NewTextHandler(f, handlerOptions)
+	slog.SetDefault(slog.New(loggerHandler))
+
 	u := linux.NewUnixRawMode()
 	restoreFunc, err = u.EnableRawMode()
 	if err != nil {
